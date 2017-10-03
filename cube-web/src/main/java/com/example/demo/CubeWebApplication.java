4bcbdb238cd4810ac8b32a815ff8a42cb1339816
Varsha Gopal <varsha.gopal@mintpayments.com>
2017-10-03 16:24:11 +1100

    Spring Cloud Config Server and Client

--- cube-web/src/main/java/com/example/demo/CubeWebApplication.java
+++ cube-web/src/main/java/com/example/demo/CubeWebApplication.java
@@ -1,20 +1,64 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class CubeWebApplication {
 
+	@Value("${app.global.message}")
+	private String globalMessage;
+
+	@Value("${app.local.message}")
+    private String localMessage;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CubeWebApplication.class, args);
 	}
-	
+
 	@RequestMapping("/api/ping")
 	public String ping() {
-		return "ping";
+		return "App's Up";
 	}
+	
+	@RequestMapping(value = "/api/message", method = RequestMethod.GET)
+    public Message getMessage() {
+        return new Message(localMessage, globalMessage);
+    }
+	
+	class Message {
+		 
+        private String localMessage;
+ 
+        private String globalMessage;
+ 
+        public Message(final String localMessage, final String globalMessage) {
+            this.localMessage = localMessage;
+            this.globalMessage = globalMessage;
+        }
+ 
+        public String getLocalMessage() {
+            return localMessage;
+        }
+ 
+        public void setLocalMessage(final String localMessage) {
+            this.localMessage = localMessage;
+        }
+ 
+        public String getGlobalMessage() {
+            return globalMessage;
+        }
+ 
+        public void setGlobalMessage(final String globalMessage) {
+            this.globalMessage = globalMessage;
+        }
+    }
 }
