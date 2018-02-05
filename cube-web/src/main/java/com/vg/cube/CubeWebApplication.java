0b8c2f52e296bc865791577733306f24b359f6d5
Varsha Gopal <varsha.gopal@mintpayments.com>
2018-02-05 14:50:01 +1100

    cloud config

--- cube-web/src/main/java/com/vg/cube/CubeWebApplication.java
+++ cube-web/src/main/java/com/vg/cube/CubeWebApplication.java
@@ -1,23 +1,15 @@
 package com.vg.cube;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.vg.cube.config.ServerConfig;
 
 @SpringBootApplication
 @RestController
-@RefreshScope
 @RequestMapping("/api")
+//@RefreshScope
 public class CubeWebApplication {
-
-    @Autowired
-    private ServerConfig serverConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(CubeWebApplication.class, args);
@@ -28,9 +20,17 @@ public class CubeWebApplication {
         return "App's Up";
     }
 
-    @RequestMapping(value = "/config", method = RequestMethod.GET)
-    public String getMessage() {
-        return serverConfig.getGlobalMessage() + " --" + serverConfig.getLocalMessage();
-    }
-
 }
+/*
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${app.global.message}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}*/
